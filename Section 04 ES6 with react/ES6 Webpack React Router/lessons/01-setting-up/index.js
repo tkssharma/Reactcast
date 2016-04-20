b1e1d0ae23f6dd13265a4273d1312e5197bf24ae
tarun <cooltarunsharma111@gmail.com>
2016-04-20 10:58:52 +0530

    section 04 updates

--- Section 04 ES6 with react/ES6 Webpack React Router/lessons/01-setting-up/index.js
+++ Section 04 ES6 with react/ES6 Webpack React Router/lessons/01-setting-up/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
+import About  from './modules/About'
+import { Router, Route, hashHistory } from 'react-router'
 
-render(<App/>, document.getElementById('app'))
+render((
+	<Router>
+	   <Route path="/"  component={App} />
+	   <Route path="/about"  component={About} />
+	</Router>
+) , document.getElementById('app'));
 
---
+++ Section 04 ES6 with react/ES6 Webpack React Router/lessons/01-setting-up/modules/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class About extends React.Component {
+     render(){
+        return <div>Hello From About  Component </div>
+     }
+}
+
+
+
