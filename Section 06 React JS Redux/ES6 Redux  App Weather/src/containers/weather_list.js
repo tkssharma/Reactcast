ad7217ab51462643d90ba803fe796a89488985aa
tarun <cooltarunsharma111@gmail.com>
2016-04-22 14:43:40 +0530

    chat sheet

--- Section 06 React JS Redux/ES6 Redux  App Weather/src/containers/weather_list.js
+++ Section 06 React JS Redux/ES6 Redux  App Weather/src/containers/weather_list.js
@@ -4,6 +4,8 @@ import Chart from '../components/chart';
 import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
+
+  
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
