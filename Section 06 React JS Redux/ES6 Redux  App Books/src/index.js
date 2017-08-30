4bb4031f86424973d709269e9eb5db1e1a14df86
tarun <tarun.softengg@gmail.com>
2017-08-30 09:52:23 +0530

    redux dev tools

--- Section 06 React JS Redux/ES6 Redux  App Books/src/index.js
+++ Section 06 React JS Redux/ES6 Redux  App Books/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
 
+import { createStore, applyMiddleware, compose } from 'redux';
+
+
+
+const store = createStore(
+	reducers,
+	compose(window.devToolsExtension ? window.devToolsExtension() : f => f )
+);
+
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.getElementById('app'));
